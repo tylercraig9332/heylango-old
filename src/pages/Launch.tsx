9ae55edb2699dd084b77e3d23a4acdba67f9be3f
Tyler Craig <tmancraig03@gmail.com>
2020-04-08 16:57:05 -0400

    Launch Page

--- src/pages/Launch.tsx
+++ src/pages/Launch.tsx
@@ -1,5 +1,45 @@
-import React from 'react' 
+import React from 'react'
+import { Row, Col, Button } from 'antd'
+import { Link } from 'react-router-dom'
 
 export default function Launch() {
-    return (<h1>Welcome to HeyLango!</h1>)
+    return (
+        <div style={pageStyle}>
+            <Row style={rowStyle}>
+                <Col span={6} offset={6} style={colStyle}>
+                    <h1 style={{color: 'dimgray'}}>Welcome to HeyLango!</h1>
+                    <p>Learn a new language today!</p>
+                    <div>
+                        <Button size="large" type="primary" block style={buttonStyle}><Link to="/signup">Sign Up!</Link></Button>
+                        <Button size="large" block style={buttonStyle}><Link to="/portal">Log In</Link></Button>
+                    </div>
+                </Col>
+            </Row>
+    </div>
+    )
+}
+
+const pageStyle = {
+    backgroundImage: 'url(static/louis-pellissier-unsplash.jpg)',
+    top: 0,
+    left: 0,
+    position: 'fixed',
+    width: '100%',
+    height: '100%',
+    zIndex: 0,
+    backgroundSize: 'cover'
+} as React.CSSProperties
+
+const rowStyle = {
+    left: 0, display: 'flex', alignItems: 'center', zIndex: 1
+} as React.CSSProperties
+
+const colStyle = {
+    minWidth: 300, marginRight: 'auto', marginLeft: 'auto', padding: 30,
+    border: '1px', borderRadius: '4px', backgroundColor: 'white',
+    marginTop: 100, zIndex: 1, textAlign: 'center'
+} as React.CSSProperties
+
+const buttonStyle = {
+    margin: 3
 }
