a9be8e7f970ab986f7be197d572175844ba1818c
tylercraig9332 <tmancraig03@gmail.com>
2021-01-02 10:53:29 -0500

    Dark Mode theme adjustment for VidLango previews

--- src/Learning/VidLango/Preview.tsx
+++ src/Learning/VidLango/Preview.tsx
@@ -4,6 +4,7 @@ import IVidLango from './VidLango'
 import { Card, message, Tag } from 'antd'
 import { parseCategoryId, parseLanguageCode, parseLanguageFlag } from '../../Util/functions'
 import { Like, Share, Save } from '../../Toolbar/Icons'
+import { useThemeSwitcher } from "react-css-theme-switcher";
 
 type PreviewProps = {
     vidLango : IVidLango
@@ -13,6 +14,7 @@ export default function Preview(props : PreviewProps) {
 
     const [hover, setHover] = useState<boolean>(false)
     const [thumb, setThumb] = useState<string>('') // tODO: replace with default url to use
+    const { currentTheme } = useThemeSwitcher()
 
     useEffect(() => {
         if (props.vidLango.meta?.thumbnails === undefined) return
@@ -30,6 +32,43 @@ export default function Preview(props : PreviewProps) {
     } else {
         captionMap = <span key="nocap" style={{margin: '0px 5px 0xp 5px'}}><Tag color="red">No Captions :(</Tag></span>
     }
+    
+    const cardWrapper = {
+        display: 'flex',
+        flexDirection: 'row',
+        margin: '20px',
+        minHeight: '300px',
+        width: '600px',
+        paddingTop: 0,
+        alignItems: 'start',
+        paddingBottom: '65px'
+    } as React.CSSProperties
+    
+    const notHover = {
+        border: `2px solid ${currentTheme === 'light' ? '#D9D9D9' : '#191C1E'}`,
+        borderRadius: '8px',
+        ...cardWrapper
+    } as React.CSSProperties
+    
+    const cardHover = {
+        border: '2px solid #1890ff',
+        borderRadius: '8px',
+        boxShadow: '2px 8px 8px fade(#1890ff, 20%)',
+        ...cardWrapper
+    } as React.CSSProperties
+    
+    const toolbar = {
+        width: '552px',
+        height: '60px',
+        display: 'flex',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        listStyle: 'none',
+        border: '1px solid #e8e8e8',
+        borderRadius: '5px',
+        zoom: 1,
+        position: 'absolute', bottom: 24 // Moves to bottom of card
+    } as React.CSSProperties
     
 
     return (
@@ -55,40 +94,3 @@ export default function Preview(props : PreviewProps) {
 function Line(props : {children : any}) {
     return <div style={{margin: 0}}>{props.children}</div>
 }
-
-const cardWrapper = {
-    display: 'flex',
-    flexDirection: 'row',
-    margin: '20px',
-    minHeight: '300px',
-    width: '600px',
-    paddingTop: 0,
-    alignItems: 'start',
-    paddingBottom: '65px'
-} as React.CSSProperties
-
-const notHover = {
-    border: '2px solid #d9d9d9',
-    borderRadius: '8px',
-    ...cardWrapper
-} as React.CSSProperties
-
-const cardHover = {
-    border: '2px solid #1890ff',
-    borderRadius: '8px',
-    boxShadow: '2px 8px 8px fade(#1890ff, 20%)',
-    ...cardWrapper
-} as React.CSSProperties
-
-const toolbar = {
-    width: '552px',
-    height: '60px',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    listStyle: 'none',
-    border: '1px solid #e8e8e8',
-    borderRadius: '5px',
-    zoom: 1,
-    position: 'absolute', bottom: 24 // Moves to bottom of card
-} as React.CSSProperties
